c4f170cb5c354dafd97facb00dd50fdc2e76dcbd
TensorFlow Datasets Team <no-reply@google.com>
2019-05-13 11:56:39 -0700

    Add so2sat benchmarking dataset with Sentinel-1 and Sentinel-2 measurments and 17 local climate zone (LCZ42) classes.
    PiperOrigin-RevId: 247981445

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -50,6 +50,7 @@ from tensorflow_datasets.image.quickdraw import QuickdrawBitmap
 from tensorflow_datasets.image.rock_paper_scissors import RockPaperScissors
 from tensorflow_datasets.image.shapes3d import Shapes3d
 from tensorflow_datasets.image.smallnorb import Smallnorb
+from tensorflow_datasets.image.so2sat import So2sat
 from tensorflow_datasets.image.sun import Sun397
 from tensorflow_datasets.image.svhn import SvhnCropped
 from tensorflow_datasets.image.voc import Voc2007
